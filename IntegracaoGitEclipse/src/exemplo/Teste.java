ce1625a8f128c49314d10e560915f6b374a6ea0d
andre.schmitt <schmitt.andre23@gmail.com>
2024-11-27 18:56:09 -0300

    commite do Andre H. Schmitt

--- IntegracaoGitEclipse/src/exemplo/Teste.java
+++ IntegracaoGitEclipse/src/exemplo/Teste.java
@@ -4,11 +4,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ALTERADO DO eclipse");
-		System.out.println("ALTERADO DO GitHub");
-		System.out.println("segundo commit");
-		System.out.println("terceiro commit");
-		System.out.println("ultimo commit");
+		System.out.println("Andre commit");
 	}
 
 }
