2a12d7ca225b3a0d35d01d7dc3fa7fa18d99e419
leandroschepers <leandrocompolt@gmail.com>
2024-11-27 18:57:02 -0300

    Primeiro commit

--- IntegracaoGitEclipse/src/exemplo/Teste.java
+++ IntegracaoGitEclipse/src/exemplo/Teste.java
@@ -3,11 +3,7 @@ package exemplo;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ALTERADO DO eclipse");
-		System.out.println("ALTERADO DO GitHub");
-		System.out.println("segundo commit");
-		System.out.println("terceiro commit");
+		System.out.println("Primerio commit");
 	}
 
 }
